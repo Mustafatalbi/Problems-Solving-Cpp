c54fe4296b7e80233e93636a2f49a5e43abf848a
MüTalbii <103079172+Mustafatalbi@users.noreply.github.com>
2022-09-05 18:22:01 +0200

    Update Age In Days.cpp

--- C++PS/Age In Days.cpp
+++ C++PS/Age In Days.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main() {
-    int Days; cin >> Days;
+    int Days; cin >> Days; // 1000
     int year = Days / 365; // convert days to years
     Days -= year*365; // 1000 - 730 = 270 
-    int months = Days / 30 ; // 1000 / 30 = 33 
-    Days -= months * 30; // 1000 - 990 = 10 
+    int months = Days / 30 ; // 270 / 30 = 9 
+    Days -= months * 30; // 270 - 270 = 0 
     int days = Days;
     
-    cout << year << endl;
-    cout << months << endl;
-    cout << days << endl;
+    cout << year << endl; // 2
+    cout << months << endl; // 9
+    cout << days << endl; // 0
     
     return 0;
 };
