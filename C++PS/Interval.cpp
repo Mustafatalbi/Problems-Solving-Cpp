a48a2177fe5606ad72481f286f0477edf21d0813
MüTalbii <103079172+Mustafatalbi@users.noreply.github.com>
2022-09-05 18:36:07 +0200

    Update Interval.cpp

--- C++PS/Interval.cpp
+++ C++PS/Interval.cpp
@@ -1,14 +1,20 @@
- #include <iostream>
- using namespace std;
+#include <iostream>
+using namespace std;
 
 int main() {
-    int x;
-    int n; cout << "Insert\n"; cin >> n; // 123 => odd
-   
-    while(n >= 10) {
-        n/=10;
-    };
-    n % 2 == 0 ? cout << "Even" : cout << "Odd";
+        float interval; cout << "Insert \n";
+        cin >> interval;
+            if (interval >= 0 && interval <= 25.00) {
+                cout << "interval (0, 25] \n";
+            } else if (interval > 25.00 && interval <= 50.00) {
+                cout << "interval (25, 50] \n";
+            } else if (interval > 50.00 && interval <= 75.00) {
+                cout << "interval (50, 75] \n";
+            } else if (interval > 75.00 && interval <= 100.0) {
+                cout << "interval (75, 100] \n";
+            } else {
+                cout << "Out Of The Rang \n";
+            }
     
     return 0;
-};
+}; 
