668e368093c49bafcf68cc18799b45dc9ccc013b
MüTalbii <103079172+Mustafatalbi@users.noreply.github.com>
2022-09-09 00:54:05 +0200

    Update Mathematical Expression.cpp

--- C++PS/Mathematical Expression.cpp
+++ C++PS/Mathematical Expression.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 using namespace std;
-
+void validate(int res, int myRes) {
+        if (res == myRes)
+            cout << "Yes";
+            else 
+            cout << "Wrong The True Is: " << myRes;
+};
 int main() {
      int a, b; char sym1, sym2; double res;
+     int myRes = 0;
      cin >> a >> sym1 >> b >> sym2 >> res; 
-     if (sym1 == '+' && sym2 == '=' && (a + b) == res) {
-         cout << "Yes \n";
-     } else if (sym1 == '-' && sym2 == '=' && (a - b) == res) {
-         cout << "Yes \n";
-     } else if (sym1 == '*' && sym2 == '=' && (a * b) == res) {
-         cout << "Yes \n";
-     } else if (sym1 == '/' && sym2 == '=' && (a / b) == res) {
-         cout << "Yes \n";
-     } else {
-       if (sym1 == '+' && sym2 == '=' && (a + b) != res) {
-           cout << "False The True Is: "<< a+b;
-       } else if (sym1 == '-' && sym2 == '=' && (a - b) != res) {
-           cout << "False The True Is: "<< a-b;
-     } else if (sym1 == '*' && sym2 == '=' && (a * b) != res) {
-           cout << "False The True Is: "<< a*b;
-     } else if (sym1 == '/' && sym2 == '=' && (a / b) != res) {
-           cout << "False The True Is: "<< (a/b);
-     };
-   
+     
+    switch(sym1) {
+        case '+': {
+            myRes = a + b;
+            validate(res, myRes);
+        } break;
+        case '-': {
+            myRes = a - b;
+            validate(res, myRes);
+        } break;
+        case '*': {
+            myRes = a * b;
+            validate(res, myRes);
+        } break;
+        case '/': {
+            myRes = a / b;
+            validate(res, myRes);
+        } break;
+    }
+    
     return 0;
-}
-}
+};
+
