adac3e947e9711c5dc39f33075fca2228135a8fa
MüTalbii <103079172+Mustafatalbi@users.noreply.github.com>
2022-09-09 23:47:16 +0200

    Add

---
+++ C++PS/Next Alphabet.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int i = 1;
+   do {
+        char c; cin >> c;
+          if (c == 'z') {
+            c = '`';
+        }
+        c++;
+         cout << c;
+   } while(i < 10);
+    return 0;
+}; 
