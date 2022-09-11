ceba97544f3a6e22c9f3b358eba97e8b9c318521
MüTalbii <103079172+Mustafatalbi@users.noreply.github.com>
2022-09-12 01:48:21 +0200

    Add

---
+++ C++PS/Fixed Password.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int a; cin >> a;
+        while(a != 1999) {
+           cout << "Wrong \n";
+                cin >> a;
+        }
+         if (a == 1999)
+             cout << "Correct \n";
+             
+    return 0;
+}; 
