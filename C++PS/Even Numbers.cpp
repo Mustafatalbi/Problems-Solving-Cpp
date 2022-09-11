a4205068b145cda32f4de10e3f7cdbaff7d84847
MüTalbii <103079172+Mustafatalbi@users.noreply.github.com>
2022-09-12 01:41:39 +0200

    Update Even Numbers.cpp

--- C++PS/Even Numbers.cpp
+++ C++PS/Even Numbers.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main() {
         int a; cin >> a;
+        if (a == 1) {
+             cout << -1;
+                return 0;
+        }; 
         for(int i=1; i<=a; i++)
             if (i % 2 == 0)
             cout << i << endl;
