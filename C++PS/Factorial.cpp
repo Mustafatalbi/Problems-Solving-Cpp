4a53767ebf3e5a9b740f22d3744828ab93c505ab
MüTalbii <103079172+Mustafatalbi@users.noreply.github.com>
2022-09-12 22:14:48 +0200

    Add

---
+++ C++PS/Factorial.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+ using namespace std;
+
+int rec(int x) {
+	if (x == 0)
+		return 1;
+  	else 
+		return x*rec(x-1);
+
+int main() {
+	int n; cin >> n;
+		cout << rec(n);
+    
+    return 0;
+};
