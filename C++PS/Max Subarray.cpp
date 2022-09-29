d3f08a821a37cea977ace23412331f6724196213
MüTalbii <103079172+Mustafatalbi@users.noreply.github.com>
2022-09-30 00:24:28 +0200

    Update Max Subarray.cpp

--- C++PS/Max Subarray.cpp
+++ C++PS/Max Subarray.cpp
@@ -1,18 +1,42 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
-
-int main() {
-        int n; cin >> n; long long c = 0;
-            for(int i=0; i<n; i++) {
-                long long val; cin >> val; // 24
-                while(val % 2 == 0) { 
-                    val/=2; 
-                    c++;
-                }
-             cout << c;
-                c = 0;
-            cout << endl;
-        }; 
-    return 0; 
+double Maximun(double x, double y);
+int main() { 
+        int test; cin >> test;
+            while(test--) {
+                int loop; cin >> loop;
+                int a[loop];
+                  for(int i=0; i<loop; i++) {
+                        cin >> a[i];
+                        cout << a[i] << " ";
+                 };
+                 int i=0, z=0, maxi;
+                 while (1) {
+                     if (z == loop - 1) 
+                         break;
+                    
+                     if (i == z) 
+                         maxi = Maximun(a[i], a[i+1]); 
+                     else 
+                         maxi = Maximun(maxi, a[i+1]);
+                
+                     cout << maxi << " ";
+                     i++;
+                     if (i == loop - 1) {
+                         z++;
+                         i = 1; // jrb i = 1;
+                         break;
+                     }
+                 }
+                 cout << endl;
+            }
+    
+    return 0;
+}; 
+double Maximun(double x, double y) {
+    if (x > y)
+        return x;
+    else 
+        return y;
 }; 
