fbaf0c874fd2574a3be4a3e86430cb9d0686b9f5
MüTalbii <103079172+Mustafatalbi@users.noreply.github.com>
2022-10-02 00:29:46 +0200

    Update O-Fibonacci.cpp

--- C++PS/O-Fibonacci.cpp
+++ C++PS/O-Fibonacci.cpp
@@ -6,7 +6,7 @@ int main()  {
                    cin >> n;
               long long f[n];
                   f[0] = 0 ; f[1] = 1; // [0,1, ]
-                      for(int i=2; i< n; i++) 
+                      for(long long i=2; i < n; i++) 
                         f[i] = f[i-1] + f[i-2]; 
                         // [0,1, ] == (i-1)(1) + (i-2)(0)
                         // 1 + 0 = 1
